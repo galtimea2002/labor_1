b6dadff17f66754193c256a5ad89309d3a5c30c4
Timi <gl.tmea90@gmail.com>
2022-10-04 11:29:26 +0300

    comit2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+/// Java-Programm, das den Durchschnitt aller ganzzahligen
+//Zahlen berechnet, die als Befehlszeilenparameter angegeben werden
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static void labor1(int a, int b){
+        System.out.println((a+b)/2);
+    }
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter a number");
+
+        int a = myObj.nextInt();  // Read user input
+
+        System.out.println("Enter another number");
+
+        int b = myObj.nextInt();
+
+        labor1(a,b);
     }
 }
